75dae5783f410173b1f94921bd26b313b6492048
naxokite <ignacio.rosario.carrera@gmail.com>
2018-11-05 15:35:45 +0100

    Adding Vigenere cracking code (utils + notebooks)

---
+++ vigenere_utils.py
@@ -0,0 +1,40 @@
+from itertools import cycle
+from six.moves import reduce
+
+ALPHA = 'abcdefghijklmnopqrstuvwxyz'
+
+
+def encrypt(key, plaintext):
+    """Encrypt the string and return the ciphertext"""
+    pairs = zip(plaintext, cycle(key))
+    result = ''
+
+    for pair in pairs:
+        total = reduce(lambda x, y: ALPHA.index(x) + ALPHA.index(y), pair)
+        result += ALPHA[total % 26]
+
+    return result.lower()
+
+
+def decrypt(key, ciphertext):
+    """Decrypt the string and return the plaintext"""
+    pairs = zip(ciphertext, cycle(key))
+    result = ''
+
+    for pair in pairs:
+        total = reduce(lambda x, y: ALPHA.index(x) - ALPHA.index(y), pair)
+        result += ALPHA[total % 26]
+
+    return result
+
+
+def show_result(plaintext, key):
+    """Generate a resulting cipher with elements shown"""
+    encrypted = encrypt(key, plaintext)
+    decrypted = decrypt(key, encrypted)
+
+    print('Key: %s' % key)
+    print('Plaintext: %s' % plaintext)
+    print('Encrytped: %s' % encrypted)
+    print('Decrytped: %s' % decrypted)
+
